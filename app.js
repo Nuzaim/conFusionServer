4c58c977d3ce6f218b5088ada74ceced173bf31c
unknown <nuzaim.t123@gmail.com>
2023-03-09 20:36:36 +0530

    Express Session Part 2

--- app.js
+++ app.js
@@ -44,42 +44,30 @@ app.use(session({
 function auth(req,res,next){
   console.log(req.session);
   if(!req.session.username){
-    var authHeader = req.headers.authorization;
-    if(!authHeader){
+    var err = new Error("You are not authenticated!");
+    res.setHeader("WWW-Authenticate","Basic");
+    err.status = 401;
+    next(err);
+    return;
+  }else{
+    if(req.session.username === "authenticated"){
+      next();
+    }else{
       var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate","Basic");
-      err.status = 401;
+      err.status = 403;
       next(err);
       return;
     }
-    var auth = new Buffer.from(authHeader.split(' ')[1],"base64").toString().split(':');
-    var username = auth[0];
-    var password = auth[1];
-    if(username==="admin" && password==="password"){
-      req.session.username = "admin";
-      next();
-    }else{
-      var err = new Error("You are not authenticated!");
-      res.setHeader("WWW-Authenticate","Basic");
-      err.status = 401;
-      next(err);
-    }
-  }else{
-    if(req.session.username === "admin"){
-      next();
-    }else{
-      var err = new Error("You are not authenticated!");
-      err.status = 401;
-      next(err);
-    }
   }
 }
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promotionRouter);
 app.use('/leaders', leaderRouter);
